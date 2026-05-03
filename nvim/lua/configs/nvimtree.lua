51707d4eb0e0e6947246822ce783d81c416707e7
dh <feed.exfoss@gmail.com>
2026-05-03 11:53:53 +0300

    current config

---
+++ nvim/lua/configs/nvimtree.lua
@@ -0,0 +1,34 @@
+return {
+    disable_netrw = true,
+    hijack_cursor = true,
+    sync_root_with_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_root = false,
+    },
+    view = {
+        width = 30,
+        preserve_window_proportions = true,
+    },
+    renderer = {
+        root_folder_label = false,
+        highlight_git = true,
+        indent_markers = { enable = true },
+        icons = {
+            git_placement = "after",
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+        },
+    },
+    filters = {
+        dotfiles = true,
+    },
+    git = {
+        enable = true,
+        ignore = true,
+    },
+}
